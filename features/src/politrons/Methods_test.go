45a6e725b29ab917e7613568f5636744fb3cd8bf
politrons <politrons@gmail.com>
2019-05-25 14:53:31 +0200

    refactor

--- features/src/politrons/Methods_test.go
+++ features/src/politrons/Methods_test.go
@@ -11,17 +11,14 @@ type MethodFoo struct {
 	name string
 }
 
-type MyList struct {
-	list []string
-}
+type MyList []string
 
 /*
 [Methods] are called in GO all functions that receice a [receiver] argument. To me it's just like
 the method extensions system that Scala use with implicits. This type of sugar syntax methods
 introduce a really powerful mechanism to create DSL and also to make the code more extensible.
 
-Method extensions only works with [type struct] and not primtives, which is not that powerful as implicts
-extensions of Scala, but it force us to create Types, which it's good.
+Method extensions only works with [type] so it force us to create Types, which it's good.
 
 it's also possible combine multiple method extensions.
 */
@@ -30,7 +27,7 @@ func TestMethodFeatures(t *testing.T){
 	newFoo := foo.upperCaseMethod()
 	fmt.Println(newFoo)
 
-	myList := MyList{[]string{"hello", "golang","bla","world","extension","methods","it`s", "cool"}}
+	myList := MyList{"hello", "golang","bla","world","extension","methods","it`s", "cool"}
 	newList := myList.deleteByIndex(2).toUpprCase()
 	fmt.Println(newList)
 }
@@ -41,18 +38,18 @@ func (foo MethodFoo) upperCaseMethod() MethodFoo{
 
 func (myList MyList) deleteByIndex(i int) MyList{
 	newList := []string{}
-	for index,value := range myList.list {
+	for index,value := range myList {
 		if(i != index){
 			newList = append(newList, value)
 		}
 	}
-	return MyList{newList}
+	return newList
 }
 
 func (myList MyList) toUpprCase() MyList{
 	newList := []string{}
-	for _,value := range myList.list {
+	for _,value := range myList {
 			newList = append(newList, strings.ToUpper(value))
 	}
-	return MyList{newList}
+	return newList
 }
