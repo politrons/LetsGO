2863c1750d6b37e42e807f44e81a067f0b465502
politrons <politrons@gmail.com>
2019-05-26 12:39:43 +0200

    refactor

--- features/src/politrons/Collections_test.go
+++ features/src/politrons/Collections_test.go
@@ -9,9 +9,6 @@ type MyFoo struct {
 	valueA string
 	valueB int
 }
-func TestArraysAndSlices(t *testing.T) {
-	arraysAndSlices()
-}
 
 //In Go a list are arrays without fixed size, just like in other languages you can have a fixed array, puting the pax size [x] 
 //when you create
@@ -19,7 +16,7 @@ func TestArraysAndSlices(t *testing.T) {
 //FP using collection and not mutate previous collection.
 //In order to  delete the slice in a FP way, we need to create a function [deleteSliceElementFunc] that recicve
 // the slice array and the element to be deleted, and return new collection without that element.
-func arraysAndSlices(){
+func TestArraysAndSlices(t *testing.T) {
 	fixedArray := [3]string{"Hello", "GoLang","World"}
 	fmt.Println(fixedArray)
 
@@ -31,36 +28,31 @@ func arraysAndSlices(){
 	fmt.Println(slice)
 	fmt.Println(newSlice)
 
-	deleteSliceElementFunc := func(_value int, slice [] int)[]int{
-		newSlice := []int{}
-		for _, value := range slice {
-			if(value != _value){
-				newSlice = append(newSlice, value)
-			}
-		}
-		return newSlice
-	}
 	newSliceDeleted := deleteSliceElementFunc(3,slice)
 	fmt.Println(newSliceDeleted)
-	//slice.add(6)
 }
-func TestMapMake(t *testing.T) {
-	mapMake()
+
+func deleteSliceElementFunc(_value int, slice [] int)[]int{
+	newSlice := []int{}
+	for _, value := range slice {
+		if(value != _value){
+			newSlice = append(newSlice, value)
+		}
+	}
+	return newSlice
 }
 
 //In GO the map collector it's created using map[keyType]valueType format.
 //To initialize the map with some values we use the operator [make]
-func mapMake() {
+func TestMapMake(t *testing.T) {
 	var myMap = make(map[string]MyFoo)
 	myMap["MyKey"] = MyFoo{"hello", 1981}
 	fmt.Println(myMap["MyKey"])
 }
-func TestMapLitertal(t *testing.T) {
-	mapLitertal()
-}
+
 //Another way to initialize maps it's using the technique Literal where after define the types as described before
 //We open brackets and we introduce one by one the key:value, key:values.
-func mapLitertal() {
+func TestMapLitertal(t *testing.T) {
 	var myMap = map[string]MyFoo{
 		"firstKey":MyFoo{"hello", 1981},
 		"secondKey":MyFoo{"golang", 2000},
@@ -70,15 +62,13 @@ func mapLitertal() {
 		fmt.Println("Key:", key, " Value:", value)
 	}
 }
-func TestMapFeatures(t *testing.T) {
-	mapFeatures()
-}
+
 //Map allow operators to add/delete elements in the collection. As OOP Language they dont embrace FP
 //So the whole collection remains mutable.
 //To get the size of a map we use operator [len] 
 //When you extract the value from a map by key, it can also return a boolean telling if the argument was
 //found or not.
-func mapFeatures() {
+func TestMapFeatures(t *testing.T) {
 	var myMap = make(map[string]MyFoo)
 	myMap["key1"] = MyFoo{"politron", 38}
 	myMap["key2"] = MyFoo{"paul", 21}
--- features/src/politrons/Types_test.go
+++ features/src/politrons/Types_test.go
@@ -43,28 +43,26 @@ type Dog struct {
 //In Go we have the same type like in the rest of the languajes. You can avoid the type declaration since is inferred by the Go compiler.
 // Instead of have double we have float of 32 or 64
 //We can have multiple declaration with different types like String, int, bool
-func primitiveTypes() bool {
+func primitiveTypes() {
 	var i int = 1
 	var f float64 = 1.1
 	var b bool = false
  	s := "Hello Go"
 	var stringVal, intVal, boolVal = "String value", 1, true
 	println(i, f, b, s, stringVal, intVal, boolVal)
-	return true
 }
 
 //Const types in Go is like create immutable types in Scala with [val] the compiler
 // it wont allow reasign a variable already asigned initiallyl
-func constTypes() bool {
+func constTypes() {
 	const stringVal, intVal, boolVal = "String value", 1, true
 	println(stringVal, intVal, boolVal)
-	return true
 }
 
 // In Go we can create types adding the key [struct] at the end of the name of the type
 // Also to define the types it use a pretty similar syntax as Haskell, we just need to use {} to define it
 // using the name of the attributes or witout it, and respeting the order of the arguments.
-func typeStruct() bool {
+func typeStruct() {
 	man := Human{
 		name:   Name("Politrons"),
 		animal: Animal{age: Age(10), sex: Sex("male")},
@@ -75,8 +73,5 @@ func typeStruct() bool {
 
 	var dog = Dog{name: Name("Bingo"), tail: 10, animal: Animal{8, "male"}}
 	fmt.Println(dog)
-	
-
 	fmt.Println(man.name + " - " + women.name)
-	return true
 }
