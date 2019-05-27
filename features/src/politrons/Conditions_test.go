4a5b9e1669dc57ea6dd3cd99e6592274fd7ab7da
nb38tv <pablo.perez.garcia@ing,com>
2019-05-27 19:00:49 +0200

    condition

--- features/src/politrons/Conditions_test.go
+++ features/src/politrons/Conditions_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 )
+
 // Unit test for the for loop functionality
 // Pretty much the same like in other languajes, quite extend and simple to use like in Scala
 func ForLoop() {
@@ -27,20 +28,20 @@ func ForLoop() {
 //Using [range] operator we can extract and interate elements from an array and get index and value.
 //You can also do it to iterte over a map and extract pair of key/value per entry.
 func TestForRange(t *testing.T) {
-	var array = []int{1,4,30,10,5,68}
-    for index,value := range array {
-		println("Index" ,index) 
-		println("Value" ,value)
+	var array = []int{1, 4, 30, 10, 5, 68}
+	for index, value := range array {
+		println("Index", index)
+		println("Value", value)
 	}
-	myMap := map[int]string{1:"value1",2:"value2",3:"value3"}
-	for key, value := range myMap{
+	myMap := map[int]string{1: "value1", 2: "value2", 3: "value3"}
+	for key, value := range myMap {
 		println("Key", key, "Value", value)
 	}
 }
 
-/* 
+/*
 [Defer] is an operator to make wait the execute of a function until the end of the current function where
-remains ends. Once the function ends start executing the defer functions just like recursively from the bottom to 
+remains ends. Once the function ends start executing the defer functions just like recursively from the bottom to
 the top of the function.
 */
 func TestForDefer(t *testing.T) {
@@ -53,4 +54,19 @@ func TestForDefer(t *testing.T) {
 	defer println("Iteration start")
 
 	fmt.Println("End")
+}
+
+/*
+A non very elegant but still effective way to make an if/else return assign the result in a immutable variable, is to create
+an anonymous function that execute and return the if/else type
+*/
+func TestIfCondition(t *testing.T) {
+	value := func() int {
+		if true {
+			return 1
+		} else {
+			return 2
+		}
+	}()
+	println(value)
 }
