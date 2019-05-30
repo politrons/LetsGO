2934d0e9943589334c7f8e7c41fd1fe127bc88bf
politrons <politrons@gmail.com>
2019-05-30 20:10:21 +0200

    DDD

--- features/src/politrons/tools/DI/application/MainApplication_test.go
+++ features/src/politrons/tools/DI/application/MainApplication_test.go
@@ -7,6 +7,16 @@ import (
 	"testing"
 )
 
+/*
+Entry point of your program, here we would receive a [Command] or [Query] and we would
+start interacting with [domain] in case of Commands, and directly with the [infrastructure] in case of Queries.
+
+Normally in DDD the application layer has dependencies with [Domain] and [Infrastructure]
+
+To achieve IOC(Inversion of control) we create an instance here of [UserRepository] from Infrastructure, and we
+set as dependency into the [Domain] which expect the interface that he define.
+Having this we achieve that the lower level of the architecture never has a dependency with the higher level.
+*/
 func TestDependencyInjectionInDDD(t *testing.T) {
 	userRepository := infrastructure.NewUserRepository()
 	entityAggregateRoot := domain.CreateEntityAggregateRoot(userRepository)
