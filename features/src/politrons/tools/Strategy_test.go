267a6151f1d42f6168c9e1bfc94b2a1f073e2a47
politrons <politrons@gmail.com>
2019-06-02 12:17:35 +0200

    Strategy pattern

--- features/src/politrons/tools/Strategy_test.go
+++ features/src/politrons/tools/Strategy_test.go
@@ -6,6 +6,9 @@ import (
 	"time"
 )
 
+//------------------------------
+// 	   USING TYPE ASSERTIONS   |
+//------------------------------
 //In this strategy we login the user using username password using same [loginUserFunc] function contract
 func TestStrategyLogin(t *testing.T) {
 	//Load strategy
@@ -50,4 +53,49 @@ func applyStrategy(strategy bool) {
 			return fmt.Sprintf("User login with token %s at loginTime %d ", token, loginTime)
 		}
 	}
+}
+
+//-------------------------
+// 	   USING INTERFACES   |
+//-------------------------
+/*
+Using type assertion allow completely cover all possible types of your system, but also lose the compilation time type check.
+We can get back that using interfaces, where we will have to implement a use case per type we want to control in our
+strategy
+*/
+func TestStrategyLoginWithInterface(t *testing.T) {
+	//Normal request
+	var typeLogin LoginUserI = UserCredentials{"politrons", "mypassword"}
+	response := typeLogin.loginUserFunc()
+	println(response)
+}
+
+//In this strategy we login the user using token and login time using same [loginUserFunc] function contract
+func TestStrategyTokenWithInterface(t *testing.T) {
+	//Normal request
+	var typeLogin LoginUserI = TokenCredentials{"FDdsgasdg787sda87987sdfa909808SADF", time.Now().UnixNano() / int64(time.Millisecond)}
+	response := typeLogin.loginUserFunc()
+	println(response)
+}
+
+type LoginUserI interface {
+	loginUserFunc() string
+}
+
+type UserCredentials struct {
+	username string
+	password string
+}
+
+type TokenCredentials struct {
+	token     string
+	loginTime int64
+}
+
+func (userCredentials UserCredentials) loginUserFunc() string {
+	return fmt.Sprintf("User login with username %s and password %s ", userCredentials.username, userCredentials.password)
+}
+
+func (tokenCredentials TokenCredentials) loginUserFunc() string {
+	return fmt.Sprintf("User login with token %s at loginTime %d ", tokenCredentials.token, tokenCredentials.loginTime)
 }
