09fa70067f5619668951dc1fea419091963c3c43
politrons <politrons@gmail.com>
2019-08-05 12:46:57 +0200

    publisher/subscriber pattern

--- features/src/politrons/tools/publisherSubscriber/PublisherSubscriberPattern_test.go
+++ features/src/politrons/tools/publisherSubscriber/PublisherSubscriberPattern_test.go
@@ -7,12 +7,37 @@ import (
 	"time"
 )
 
+//##################################//
+//	PUBLISHER/SUBSCRIBER PATTERN  //
+//##################################//
+
+/**
+Publisher subscriber pattern is also know as event-driven, is a new reactive way to communicate between
+[producer/publisher] and [consumer/subscriber].
+
+The pattern it was design due of the need to solve one of the most commons problems in backend, (Out of memory) basically overwhelm a backend
+with messages when it's not possible to process those messages. This need brings the concept of [Back-pressure] which basically solve this problem
+changing the old fashion [iterable] by [pull] in the communications. Where instead the publisher send all the message that has, they wait until
+the client make pull and tell him that he is ready to receive more messages.
+
+The design it divide in two parts, the [publisher], [observable] and [subscriber].
+*/
+
+/**
+For this test, we create a [Publisher] and we start appending Events in the [channel] of Events with a production of 100 ms (Higher than what consumer can process)
+
+Then we create an [Observable] passing the publisher and we [subscribe] passing three functions that they will be invoked by the Observable, for a specific state.
+Those functions are:
+	[doOnNext(event Event)]: A consumer function that receive the event passed from the publisher to do something.
+	[doOnError(error Error)]: A consumer function that receive an error in the emission and process of one of the events
+	[doOnComplete()]: A function with no arguments that it's invoked when there's no more events from the publisher and the emissions is finish.
+*/
 func TestPublisherSubscriberPattern(t *testing.T) {
 	publisher := Publisher{"1000", make(chan Event)}
 
 	index := 0
 	go func() {
-		for index < 100 {
+		for index < 10 {
 			publisher.appendEvent(Event{"message:" + strconv.Itoa(index)})
 			index++
 			time.Sleep(100 * time.Millisecond)
@@ -97,7 +122,7 @@ func (observable Observable) subscribe(onNext func(event Event), onError func(er
 		} else {
 			subscriber.doOnNext(event)
 		}
-		time.Sleep(200 * time.Millisecond) // business logic delay (Connection with other backend and so on)
+		time.Sleep(500 * time.Millisecond) // business logic delay (Connection with other backend and so on)
 	}
 }
 
