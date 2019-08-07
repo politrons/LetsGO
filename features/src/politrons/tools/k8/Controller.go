e2df66c97d261a13ab0181a387b6067255efbce2
politrons <politrons@gmail.com>
2019-08-07 10:25:22 +0200

    Add Kubernetes Operator

---
+++ features/src/politrons/tools/k8/Controller.go
@@ -0,0 +1,124 @@
+package k8
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"k8s.io/api/core/v1"
+	rbacV1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+// NamespaceController watches the kubernetes api for changes to namespaces and
+// creates a RoleBinding for that particular namespace.
+type NamespaceController struct {
+	namespaceInformer cache.SharedIndexInformer
+	kclient           *kubernetes.Clientset
+}
+
+// NewNamespaceController creates a new NewNamespaceController
+func NewNamespaceController(kclient *kubernetes.Clientset) *NamespaceController {
+	namespaceWatcher := &NamespaceController{}
+
+	// Create informer for watching Namespaces
+	namespaceInformer := cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return kclient.CoreV1().Namespaces().List(options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return kclient.CoreV1().Namespaces().Watch(options)
+			},
+		},
+		&v1.Namespace{},
+		3*time.Minute,
+		cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc},
+	)
+
+	namespaceInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: namespaceWatcher.createRoleBindingByNamespace,
+	})
+
+	namespaceWatcher.kclient = kclient
+	namespaceWatcher.namespaceInformer = namespaceInformer
+
+	return namespaceWatcher
+}
+
+func (controller *NamespaceController) createRoleBindingByNamespace(obj interface{}) {
+	namespaceObj := obj.(*v1.Namespace)
+	namespaceName := namespaceObj.Name
+
+	roleBinding := controller.createRoleBindingInfo(namespaceName)
+
+	controller.addRoleBindingInNameSpace(namespaceName, roleBinding)
+	controller.deleteRoleBindingInNameSpace(namespaceName, roleBinding)
+
+}
+
+//Create the roleBinding to pass later to RoleBindings creation
+func (controller *NamespaceController) createRoleBindingInfo(namespaceName string) *rbacV1.RoleBinding {
+	roleBinding := &rbacV1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "RoleBinding",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("ad-kubernetes-%s", namespaceName),
+			Namespace: namespaceName,
+		},
+		Subjects: []rbacV1.Subject{
+			{
+				Kind: "Group",
+				Name: fmt.Sprintf("ad-kubernetes-%s", namespaceName),
+			},
+		},
+		RoleRef: rbacV1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "edit",
+		},
+	}
+	return roleBinding
+}
+
+func (controller *NamespaceController) addRoleBindingInNameSpace(namespaceName string, roleBinding *rbacV1.RoleBinding) {
+	_, err := controller.kclient.RbacV1().RoleBindings(namespaceName).Create(roleBinding)
+	if err != nil {
+		log.Println(fmt.Sprintf("Failed to create Role Binding: %s", err.Error()))
+	} else {
+		log.Println(fmt.Sprintf("Created AD RoleBinding for Namespace: %s", roleBinding.Name))
+	}
+}
+
+func (controller *NamespaceController) deleteRoleBindingInNameSpace(namespaceName string, roleBinding *rbacV1.RoleBinding) {
+	var options *metav1.DeleteOptions
+	err := controller.kclient.RbacV1().RoleBindings(namespaceName).Delete(fmt.Sprintf("ad-kubernetes-%s", namespaceName), options)
+	if err != nil {
+		log.Println(fmt.Sprintf("Failed to delete Role Binding: %s", err.Error()))
+	} else {
+		log.Println(fmt.Sprintf("Delete AD RoleBinding for Namespace: %s", roleBinding.Name))
+	}
+}
+
+// Run starts the process for listening for namespace changes and acting upon those changes.
+func (controller *NamespaceController) Run(stopCh <-chan struct{}, wg *sync.WaitGroup) {
+	// When this function completes, mark the go function as done
+	defer wg.Done()
+
+	// Increment wait group as we're about to execute a go function
+	wg.Add(1)
+
+	// Execute go function
+	go controller.namespaceInformer.Run(stopCh)
+
+	// Wait till we receive a stop signal
+	stopSignal := <-stopCh
+	log.Println(fmt.Sprintf("Stop signal from : %s", stopSignal))
+}
