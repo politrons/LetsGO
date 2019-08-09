6234fa72b5bea18e19f39a3936e443b516a88ee6
politrons <politrons@gmail.com>
2019-08-09 10:37:05 +0200

    Add post controller

--- features/src/politrons/tools/k8/pod/PodController.go
+++ features/src/politrons/tools/k8/pod/PodController.go
@@ -7,6 +7,15 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+/**
+Pods are the smallest deployable units of computing that can be created and managed in Kubernetes
+
+A Pod is a group of one or more containers(docker), with shared storage/network,
+and a specification for how to run the containers.
+
+You can run a shell in the Pod using command [kubectl exec -it your-pod-name -- /bin/bash]
+*/
+
 type Controller struct {
 	kclient *kubernetes.Clientset
 }
@@ -33,6 +42,12 @@ func (controller *Controller) GetAllPods(namespace string) ([]v1.Pod, error) {
 	return podList.Items, err
 }
 
+/**
+Function to create a new [Pod] for the creation of the Pod like in other K8s components like RoleBinding, we need
+to pass the instance of the type to persist, in this case [Pod]
+Then using a namespace name that already exist we use the API [CoreV1().Pods(namespace).Create]
+passing the pod.
+*/
 func (controller *Controller) CreatePod(namespace string) (*v1.Pod, error) {
 	podInfo := controller.createPodInfo(namespace)
 	pod, err := controller.kclient.CoreV1().Pods(namespace).Create(podInfo)
@@ -43,6 +58,9 @@ func (controller *Controller) CreatePod(namespace string) (*v1.Pod, error) {
 	return pod, err
 }
 
+/**
+Function to delete a Pod using the namespace name where is running and the name of the pod.
+*/
 func (controller *Controller) DeletePod(namespace string, podName string) (bool, error) {
 	var deleteOptions *metav1.DeleteOptions
 	err := controller.kclient.CoreV1().Pods(namespace).Delete(podName, deleteOptions)
@@ -53,6 +71,14 @@ func (controller *Controller) DeletePod(namespace string, podName string) (bool,
 	return true, err
 }
 
+/**
+Factory function to create an instance of type [Pod].
+We provide some info of the pod using [TypeMeta], and also detail of the pod info using [ObjectMeta]
+
+Finally and most important we define the [PodSpec] where we describe the array of containers that the Pod
+contains. It's an array of [Container] which each has a name and image. The pod it will pull the image from
+docker-hub and it will run inside the pod.
+*/
 func (controller *Controller) createPodInfo(namespace string) *v1.Pod {
 	return &v1.Pod{TypeMeta: metav1.TypeMeta{
 		Kind:       "pod",
@@ -65,8 +91,8 @@ func (controller *Controller) createPodInfo(namespace string) *v1.Pod {
 		Spec: v1.PodSpec{
 			Containers: []v1.Container{
 				{
-					Name:  "nginx",
-					Image: "nginx",
+					Name:  "nginx", //
+					Image: "nginx", //Image you want to pull and run
 				},
 			},
 		},
